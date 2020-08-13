f40a9ae58dc7817e2c2cc95d7d98838448d74ac6
Soph95 <64817554+Soph95@users.noreply.github.com>
2020-08-13 21:02:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function bmiCalculator() {
 
-  var weight = document.getElementById("weight").value;
-  var height = document.getElementById("height").value;
+  var weight = document.querySelector(".weight").value;
+  var height = document.querySelector(".height").value;
   var bmi = weight / Math.pow(height, 2);
   var bmifinal = (Math.round(bmi * 100) / 100).toFixed(1);
 
@@ -12,14 +12,15 @@ function bmiCalculator() {
 
 
   if (bmifinal < 18.5) {
-    document.getElementById("result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as underweight.";
+    document.querySelector(".result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as underweight.";
   } else if (bmifinal >= 18.5 && bmi <= 24.9) {
-    document.getElementById("result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as normal weight.";
+    document.querySelector(".result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as normal weight.";
   } else if (bmifinal > 24.9 && bmifinal <= 29.9) {
-    document.getElementById("result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as overweight.";
+    document.querySelector(".result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as overweight.";
   } else if (bmifinal >= 30) {
-    document.getElementById("result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as obese.";
+    document.querySelector(".result").innerHTML = "Your BMI is " + bmifinal + "," + " so you are classed as obese.";
   }
 
 }
 document.querySelector("button").addEventListener("click", bmiCalculator);
+
